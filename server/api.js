ae0cbf8bd7b695c7ca443d138cefddbe3d523a07
sin <must.sow.before.you.can.reap+0001@gmail.com>
2021-10-07 10:15:42 +0900

    fix error

--- server/api.js
+++ server/api.js
@@ -1,8 +1,13 @@
 module.exports = (router, controller, resources, idName) => {
   const root = `/api/${controller.name}`;
   const resource = `:${idName || `${controller.name}Id`}`;
-  const able = (actionName) => !resources || resources.includes(actionName);
+  const able = (actionName) =>
+    (!resources || resources.includes(actionName)) &&
+    controller.hasOwnProperty(actionName);
 
   if (able('index')) router.get(root, controller.index);
+  if (able('create')) router.get(root, controller.create);
   if (able('show')) router.get(`${root}/${resource}`, controller.show);
+  if (able('update')) router.get(`${root}/${resource}`, controller.update);
+  if (able('destroy')) router.get(`${root}/${resource}`, controller.destroy);
 };
