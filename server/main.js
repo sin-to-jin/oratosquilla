6d8ace2d9eba255a7b1db9f80b11e9f6bd0565a3
sin <must.sow.before.you.can.reap+0001@gmail.com>
2021-10-07 23:53:58 +0900

    support http parameter pollution attack

--- server/main.js
+++ server/main.js
@@ -1,16 +1,27 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
-const logger = require('./logger.js');
-const router = require('./routes.js');
+const hpp = require('hpp');
+const rateLimit = require('express-rate-limit');
 const app = express();
-const port = 3001;
 
 app.use(cors());
 app.use(helmet());
+app.use(hpp());
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
+app.use(limiter);
+
+const logger = require('./logger.js');
 app.use(logger);
+
+const router = require('./routes.js');
 app.use(router);
 
+const port = 3001;
 app.listen(port, () => {
   console.log(`Server listen at http://localhost:${port}`);
 });
